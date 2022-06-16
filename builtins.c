3d310ff92341797ed0108349dba0242b02b0b19a
Olise <you@example.abanum00@gmail.com>
2022-06-15 19:10:01 -0700

    builtin error correction

--- builtins.c
+++ builtins.c
@@ -34,31 +34,29 @@ void (*check_for_builtins(vars_t *vars))(vars_t *vars)
  */
 void new_exit(vars_t *vars)
 
-
-
+{
 	int status;
 
-
 	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
-{
-	status = _atoi(vars->av[1]);
-	if (status == -1)
 	{
-		vars->status = 2;
-		print_error(vars, ": Illegal number: ");
-		_puts2(vars->av[1]);
-		_puts2("\n");
-		free(vars->commands);
-		vars->commands = NULL;
-		return;
+		status = _atoi(vars->av[1]);
+		if (status == -1)
+		{
+			vars->status = 2;
+			print_error(vars, ": Illegal number: ");
+			_puts2(vars->av[1]);
+			_puts2("\n");
+			free(vars->commands);
+			vars->commands = NULL;
+			return;
+		}
+		vars->status = status;
 	}
-	vars->status = status;
-}
-free(vars->buffer);
-free(vars->av);
-free(vars->commands);
-free_env(vars->env);
-exit(vars->status);
+	free(vars->buffer);
+	free(vars->av);
+	free(vars->commands);
+	free_env(vars->env);
+	exit(vars->status);
 }
 
 /**
